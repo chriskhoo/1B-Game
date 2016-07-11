c9951d8c38a9e3965945a9edf2987f7704cbf92b
Christopher Khoo <khooweiwen@gmail.com>
2016-07-12 00:35:17 +0800

    wall and paddle bounce

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
   // object with variables for game frame
   var gameVar = {
     // canvas size
-    WIDTH : 700,
-    HEIGHT : 600,
+    canvasWidth : 700,
+    canvasHeight : 600,
     pi: Math.PI,
     // need a canvas and ctx variable to draw rectangles
     canvas : null,
@@ -68,12 +68,9 @@ $(document).ready(function() {
     width: 20,
     height: 20,
     speed: 0,
+    velocity: 0,
     alpha: gameVar.pi * 0.2,
-
-    // vel : {
-    //  x: this.speed*Math.cos(this.alpha),
-    //  y: this.speed*Math.sin(this.alpha)
-    // },
+    paddle: 0,
 
     // function to draw the player's paddle
     draw: function(){
@@ -84,10 +81,81 @@ $(document).ready(function() {
     update: function(){
     //  console.log("update ball");
 
-    this.x += this.vel.x;
-    this.y += this.vel.y;
+    // ball moves according to velocity vectors
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+
+    this.wallBounce();
+    this.paddleIntersectionCheck();
+    this.checkPlayer();
+    this.paddleIntersection();
+
+  },
+
+    // ball bounces off walls
+    wallBounce: function(){
+      if( this.y < 0 || this.y+this.height > gameVar.canvasHeight){
+        // compensate for distance travelled past borders
+        var offset;
+        if(this.velocity.y<0){
+          offset = 0 - this.y;
+        }
+        else{
+          offset = gameVar.canvasHeight - this.y - this.height;
+        }
+        this.y += 2 * offset;
+
+        // perfect elastic collision
+        this.velocity.y *= -1;
+      }
+    },
+
+    // ball bouncing off paddles
+    paddleIntersectionCheck: function(paddlex, paddley, paddleWidth, paddleHeight, ballx, bally, ballWidth, ballHeight){
+      if(paddlex < ballx+ballWidth && paddlex + paddleWidth > ballx && paddley < bally+ballHeight  && paddley + paddleHeight > bally){
+        return true;
+      }
+      else{
+          return false;
+      }
+    },
+
+    // check which paddle
+    checkPlayer: function(){
+      if(this.velocity.x > 0){
+        this.paddle = player2;
+      }
+      else{
+        this.paddle = player1;
+      }
+    },
+
+    // paddle bounce
+    paddleIntersection: function(){
+      if (this.paddleIntersectionCheck(this.paddle.x, this.paddle.y, this.paddle.width, this.paddle.height, this.x, this.y, this.width, this.height)){
+        // perpendicular bounce
+        //  this.velocity.x *= -1;
+
+        // calculates relative position of ball on paddle's height
+        var ballvsPaddleHeight = (this.y+this.height-this.paddle.y)/(this.paddle.height+this.height);
+
+        /// gives 45 degree if ball hits the edges of the paddle and 90 degree if it hits the middle
+        this.alpha = ( (2*ballvsPaddleHeight)-1 ) * gameVar.pi /4;
+        this.speed = Math.sqrt(this.velocity.x*this.velocity.x+this.velocity.y*this.velocity.y);
+        this.velocity.y = this.speed*Math.sin(this.alpha);
+      //
+        if(this.velocity.x > 0){
+        this.velocity.x = this.speed*Math.cos(this.alpha)*(-1);
+        this.x = player2.x - this.width;
+      }
+        else{
+        this.velocity.x = this.speed*Math.cos(this.alpha)*(1);
+        this.x += player1.x + player1.width;
+        }
+      }
 
     }
+
 
   };
 
@@ -95,12 +163,12 @@ $(document).ready(function() {
   var net = {
     w : 4,
     y : 0,
-    x : (gameVar.WIDTH - 4)/2,
-    step: gameVar.HEIGHT/20,
+    x : (gameVar.canvasWidth - 4)/2,
+    step: gameVar.canvasHeight/20,
 
     // function to draw the net
     draw: function(){
-      while(this.y<gameVar.HEIGHT){
+      while(this.y<gameVar.canvasHeight){
       gameVar.ctx.fillRect(this.x, this.y, this.w, this.step/2);
       this.y += this.step;
     }
@@ -112,8 +180,8 @@ $(document).ready(function() {
   function main() {
     // create a canvas element in HTML
     gameVar.canvas = document.createElement("canvas");
-    gameVar.canvas.width = gameVar.WIDTH;
-    gameVar.canvas.height = gameVar.HEIGHT;
+    gameVar.canvas.width = gameVar.canvasWidth;
+    gameVar.canvas.height = gameVar.canvasHeight;
     gameVar.ctx = gameVar.canvas.getContext("2d");
     document.body.appendChild(gameVar.canvas);
 
@@ -137,7 +205,7 @@ $(document).ready(function() {
       window.requestAnimationFrame(loop);
     };
     loop();
-    console.log(ball.vel);
+    console.log(ball.velocity);
     // draw();
 
   }
@@ -147,17 +215,17 @@ $(document).ready(function() {
     console.log('game initailized');
     // initial values for moving elements
     player1.x = player1.width;
-    player1.y = (gameVar.HEIGHT-player1.height)/2;
+    player1.y = (gameVar.canvasHeight-player1.height)/2;
 
-    player2.x = gameVar.WIDTH-player1.width-player2.width;
-    player2.y = (gameVar.HEIGHT-player2.height)/2;
+    player2.x = gameVar.canvasWidth-player1.width-player2.width;
+    player2.y = (gameVar.canvasHeight-player2.height)/2;
 
-    ball.x = (gameVar.WIDTH-ball.width)/2;
-    ball.y = (gameVar.HEIGHT-ball.height)/2;
+    ball.x = (gameVar.canvasWidth-ball.width)/2;
+    ball.y = (gameVar.canvasHeight-ball.height)/2;
 
-    ball.vel = {
-      x : 0,
-      y : -10
+    ball.velocity = {
+      x : 10,
+      y : 0
     };
   }
 
@@ -173,7 +241,7 @@ $(document).ready(function() {
   //  console.log('draw box');
   // screen
     gameVar.ctx.fillStyle = "#000000";
-    gameVar.ctx.fillRect(0,0,gameVar.WIDTH,gameVar.HEIGHT);
+    gameVar.ctx.fillRect(0,0,gameVar.canvasWidth,gameVar.canvasHeight);
 
   // draw new positions of ball and players
     gameVar.ctx.fillStyle = "#ffffff";
