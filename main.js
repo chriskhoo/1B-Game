0b5fb068841711e3b744bba3c733f2aef0a9904a
Christopher Khoo <khooweiwen@gmail.com>
2016-07-11 12:29:40 +0800

    file setup

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  console.log('JS linked');
+
+
+  function gameInit() {
+  console.log('game initailized');
+  }
+
+  // stopwatch.init();
+  gameInit();
+
+
+
+});
