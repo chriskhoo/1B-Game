bbf0efbd18c4b049454ac9da277e109ec5a4172d
Christopher Khoo <khooweiwen@gmail.com>
2016-07-11 21:10:47 +0800

    add paddle movement and animation

--- main.js
+++ main.js
@@ -3,32 +3,39 @@ $(document).ready(function() {
 
   // object with variables for game frame
   var gameVar = {
+    // canvas size
     WIDTH : 700,
     HEIGHT : 600,
     pi: Math.PI,
     // need a canvas and ctx variable to draw rectangles
     canvas : null,
     ctx: null,
-    keystate: false,
+    // define an object for the keystates
+    keystate: {},
   };
 
   // object with variables for player with optional up and down arrows
   var player1 = {
-    // variables for player 1 paddle
+    // variables for player 1 paddle size
     x: null,
     y: null,
     width: 20,
     height: 100,
+    // define player 1 keystates
+    upKey : 87,
+    downKey : 83,
     // optional name
     // optional keystrokes
     draw: function(){
       // function to draw the player's paddle
-        console.log("draw player1");
+      //  console.log("draw player1");
         gameVar.ctx.fillRect(this.x,this.y,this.width,this.height);
       },
     update: function(){
       // function to move player paddle
-      console.log("update player1");
+    //  console.log("update player1");
+    if(gameVar.keystate[this.upKey]) this.y -= 7;
+    if(gameVar.keystate[this.downKey]) this.y += 7;
     }
   };
 
@@ -38,13 +45,18 @@ $(document).ready(function() {
     y: null,
     width: 20,
     height: 100,
+    // define player 2 keystates
+    upKey : 38,
+    downKey : 40,
 
     draw: function(){
-        console.log("draw player2");
+      //  console.log("draw player2");
         gameVar.ctx.fillRect(this.x,this.y,this.width,this.height);
       },
     update: function(){
-      console.log("update player2");
+    //  console.log("update player2");
+    if(gameVar.keystate[this.upKey]) this.y -= 7;
+    if(gameVar.keystate[this.downKey]) this.y += 7;
     }
   };
 
@@ -55,13 +67,13 @@ $(document).ready(function() {
     y: null,
     width: 20,
     height: 20,
-    speed: 10,
-    alpha: 0,
+    speed: 0,
+    alpha: gameVar.pi * 0.2,
 
-    vel : {
-     x: this.speed*Math.cos(this.alpha),
-     y: this.speed*Math.sin(this.alpha)
-    },
+    // vel : {
+    //  x: this.speed*Math.cos(this.alpha),
+    //  y: this.speed*Math.sin(this.alpha)
+    // },
 
     // function to draw the player's paddle
     draw: function(){
@@ -70,7 +82,11 @@ $(document).ready(function() {
 
     // function to move ball
     update: function(){
-      console.log("update ball");
+    //  console.log("update ball");
+
+    this.x += this.vel.x;
+    this.y += this.vel.y;
+
     }
 
   };
@@ -88,6 +104,7 @@ $(document).ready(function() {
       gameVar.ctx.fillRect(this.x, this.y, this.w, this.step/2);
       this.y += this.step;
     }
+    this.y =0;
     }
   };
 
@@ -100,10 +117,28 @@ $(document).ready(function() {
     gameVar.ctx = gameVar.canvas.getContext("2d");
     document.body.appendChild(gameVar.canvas);
 
+    // activate keystate listeners
+    document.addEventListener("keydown", function(evt){
+      gameVar.keystate[evt.keyCode] = "true";
+      }
+    );
+    document.addEventListener("keyup", function(evt){
+      delete gameVar.keystate[evt.keyCode];
+      }
+    );
+
     //initialize game
     gameInit();
 
-    draw();
+    // run the game & continuously update the visuals
+    var loop = function (){
+      update();
+      draw();
+      window.requestAnimationFrame(loop);
+    };
+    loop();
+    console.log(ball.vel);
+    // draw();
 
   }
 
@@ -119,11 +154,15 @@ $(document).ready(function() {
 
     ball.x = (gameVar.WIDTH-ball.width)/2;
     ball.y = (gameVar.HEIGHT-ball.height)/2;
+
+    ball.vel = {
+      x : 0,
+      y : -10
+    };
   }
 
   // update functions
   function update(){
-    console.log('game updated');
     ball.update();
     player1.update();
     player2.update();
@@ -131,7 +170,7 @@ $(document).ready(function() {
 
 
   function draw(){
-    console.log('draw box');
+  //  console.log('draw box');
   // screen
     gameVar.ctx.fillStyle = "#000000";
     gameVar.ctx.fillRect(0,0,gameVar.WIDTH,gameVar.HEIGHT);
@@ -141,10 +180,9 @@ $(document).ready(function() {
     ball.draw();
     player1.draw();
     player2.draw();
+
     net.draw();
-
-
-  }
+    }
 
   // launch game
   main();
